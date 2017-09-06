9c103575ca9788e16daf9f167ab3454fc219d8be
Tom Ballinger <escher00@yahoo.com>
2017-09-06 07:24:36 -0400

    one more time

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end("Hello World\nIT's TUESDAY\nand not working :-(\n")
+   response.end("Hello World\nIT's TUESDAY\n")
 }).listen(3000)
 
 // Console will print the message
