8e2be3b9377d28efcd65bfc548171fc9d26abed6
Tom Ballinger <escher00@yahoo.com>
2017-09-08 12:23:51 -0400

    message change test

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end("Hello World\nIT's TUESDAY\n")
+   response.end("Hello World\nIT's TUESDAY\nNope--It's FRIDAY")
 }).listen(3000)
 
 // Console will print the message
