34b25557af7a6e6b7209ded5d2ca40bf28404711
Tom Ballinger <escher00@yahoo.com>
2017-09-11 13:04:30 -0400

    back to monday

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end("Hello World\nIt's FRIDAY\nFINALLY\n")
+   response.end("Hello World\nIt's back to Monday\n")
 }).listen(3000)
 
 // Console will print the message
